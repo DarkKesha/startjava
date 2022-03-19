f68cd6b4d79b544578041f79d4129e5d2aa26cbd
DaniilGorodilov <gorod.dan@mail.ru>
2022-03-19 14:16:01 +1000

    Расчет стоимости товара со скидкой

--- VariablesTheme.java
+++ VariablesTheme.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class VariablesTheme {
+public class VariablesTheme {
+    public static void main(String[] args) {
+        byte a = 64;
+        short b = 327;
+        int c = 12345;
+        long d = 123456;
+        float i = 922337203;
+        double f = 1.5;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(i);
+        System.out.println(f);
+
+        int x = 100;
+        int y = 200;
+        int z = ((x + y) * 11) / 100;
+        System.out.println(x + y - z);
+        System.out.println(z);
+    }
 }
